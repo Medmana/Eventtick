783dbefce7290d4e91ffcf49b2b15ea898556dd0
Medmana <ahiffonman@gmail.com>
2025-01-30 12:25:27 +0100

    cc

--- src/app.js
+++ src/app.js
@@ -16,6 +16,10 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log(process.env.NODE_ENV);
 
 // Utilisation de __dirname maintenant définie
 console.log(__dirname);
