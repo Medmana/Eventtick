cf9487c2f91add3039320430e96ebd6e58c0f75a
Medmana <ahiffonman@gmail.com>
2025-01-30 11:25:52 +0100

    hum

--- src/authentication.js
+++ src/authentication.js
@@ -1,26 +1,25 @@
+import { hooks } from '@feathersjs/authentication';
 import pkg from '@feathersjs/authentication-local';
 const { LocalStrategy } = pkg;
-import { hooks } from '@feathersjs/authentication';
 
-export const authentication = app => {
-  // Configure le service d'authentification
+export const configureAuthentication = (app) => {
+  // Configuration de la stratégie d'authentification locale
   app.configure(authentication({
-    // Spécifie les stratégies d'authentification
     local: new LocalStrategy()
   }));
 
-  // Ajoute les hooks nécessaires à l'authentification
+  // Ajouter des hooks à la service 'authentication'
   app.service('authentication').hooks({
     before: {
       create: [
-        hooks.authenticate('local')
+        hooks.authenticate('local')  // Utilisation de la stratégie 'local' pour l'authentification
       ]
     }
   });
 
-  // Définit le champ de l'utilisateur pour la stratégie locale
+  // Définir les champs pour 'username' et 'password'
   app.get('authentication').local = {
-    usernameField: 'email',  // Utilise 'email' pour l'authentification
-    passwordField: 'password'  // Champ pour le mot de passe
+    usernameField: 'email', // Utilisation du champ 'email' pour l'authentification
+    passwordField: 'password'
   };
 };
