86e852bf2ffd5ef6e409dcf8ee500fe5af14f0a0
Medmana <ahiffonman@gmail.com>
2025-01-30 14:44:03 +0100

    on

--- api/src/services/index.js
+++ api/src/services/index.js
@@ -1,17 +1,9 @@
-import { evs } from './evs/evs.js'
-import { achatickets } from './achatickets/achatickets.js'
-import { tickets } from './tickets/tickets.js'
-import { categories } from './categories/categories.js'
+
 import { evenements } from './evenements/evenements.js'
 import { user } from './users/users.js'
 export const services = (app) => {
-  app.configure(evs)
+ 
 
-  app.configure(achatickets)
-
-  app.configure(tickets)
-
-  app.configure(categories)
 
   app.configure(evenements)
 
